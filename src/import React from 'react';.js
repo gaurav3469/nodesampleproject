165a06c5182e7d6d247f088e22e4ea5a6ea34591
gaurav3469 <gauravkumar3469@gmail.com>
2021-01-23 18:14:52 +0530

    firstpush

---
+++ src/import React from 'react';.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root') `13 s`
+);
+
+// If you want to a44444444444444444444444444444444444444444444444444444444444444444444444444444444444444tart measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
